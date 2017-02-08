f5582f2c88253c207a4421bd777601ef62468b47
TrAnMu <Travis@wireless-10-105-6-217.umd.edu>
2017-02-08 18:43:58 -0500

    Now THAT'S a goal

--- Tilt Maze/Assets/Scripts/Ball.cs
+++ Tilt Maze/Assets/Scripts/Ball.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (rb.isKinematic == true) {
+			transform.Translate (Vector3.up * Time.deltaTime, Space.World);
+		}
+
+		if (Input.GetKey ()) {
+
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.name == "Goal") {
-			Destroy (gameObject, 0.7f);
-			Debug.Log ("You win!");
+			rb.isKinematic = true;
 		}
 	}
 }
